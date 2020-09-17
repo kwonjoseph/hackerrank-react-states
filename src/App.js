ac376aeb22fad1b7fe68691771e04c775948dc31
emilygarvey <emilyg@scripted.org>
2020-09-17 17:01:18 -0400

    IceCream component question

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
 import "./App.css";
 
-const App = () => {
-  return (<div></div>);
+/*
+1. Create a new functional component called IceCream that returns a div tag
+2. Inside the div tag, create a paragraph tag that displays a flavor using props
+  Name your prop 'flavor'
+3. In App, add three IceCream components that receive your flavor props strawberry, chocolate, and vanilla
+*/
+
+
+
+
+function App() {
+  return (
+  <div className="app">
+    <h1>Menu Choices</h1>
+
+  </div>
+  );
 };
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import App, { IceCream } from "./App";
 
 /**************************************************************************************************
  * Volunteers: add your tests here. They should be failing, but the students should be able to make
@@ -8,10 +9,38 @@ import "@testing-library/jest-dom/extend-expect";
  **************************************************************************************************/
 
 
-describe("Tests", () => {
-  test("blank test",()=>{
-    // write tests here. 
-    expect(true).toBe(false);
+describe("when rendering the App component", () => {
+  test("IceCream can render a flavor prop",()=>{
+    const { getByText } = render(<IceCream flavor='foo' />);
+    const textElement = getByText('foo');
+    expect(textElement).toBeInTheDocument();
+  })
+  test("App will have strawberry IceCream component",()=>{
+    const { getByText } = render(<App />)
+    const strawberry = getByText("strawberry");
+    expect(strawberry).not.toBeFalsy();
+  })
+  test("App will have chocolate IceCream component",()=>{
+    const { getByText } = render(<App />)
+    const chocolate = getByText("chocolate");
+    expect(chocolate).not.toBeFalsy();
+  })
+  test("App will have vanilla IceCream component",()=>{
+    const { getByText } = render(<App />)
+    const vanilla = getByText("vanilla");
+    expect(vanilla).not.toBeFalsy();
   })
 });
 
+/**
+ * test('loads and displays greeting', async () => {
+  render(<Fetch url="/greeting" />)
+
+  fireEvent.click(screen.getByText('Load Greeting'))
+
+  await waitFor(() => screen.getByRole('heading'))
+
+  expect(screen.getByRole('heading')).toHaveTextContent('hello there')
+  expect(screen.getByRole('button')).toHaveAttribute('disabled')
+})
+ */
