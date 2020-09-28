990fcbd94817c7dade6f32e1641d89e7b617a16e
Joseph Kwon <kwonjosepho@gmail.com>
2020-09-28 04:47:40 -0700

    initial commit to add question for event handler

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "./App.css";
 
 /*
-1. Create a new functional component called IceCream that returns a div tag
-2. Inside the div tag, create a paragraph tag that displays a flavor using props
-  Name your prop 'flavor'
-3. In App, add three IceCream components that receive your flavor 
-  props strawberry, chocolate, and vanilla
+1. Add an event handler that will change the state of the diamond into a circle.
+2. I have already preset some css into the App.css file to reflect the diamond properties, but you will need to make it transform.
+3. add an event handler to the button to change the state of the diamond to a circle and back to a diamond if pressed again.
+4. use the border-radius css property and set it to '50%' to change the div into a circle.
 */
-
-
-
-
 function App() {
+const [ borderRadius, setBorderRadius ] = useState('0%')
   return (
-  <div className="app">
-    <h1>Menu Choices</h1>
-
-  </div>
-  );
-};
+    <div className="app">
+      <div className="diamond" style={{backgroundColor: '#b9f2ff', borderRadius: borderRadius}} data-testid='object'></div>
+      <button onClick={() => null}>morpher</button>
+    </div>
+    );
+}
 
 export default App;
